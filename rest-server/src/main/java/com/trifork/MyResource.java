7b28d4772354508dbe781e41c3bd7f281af3c1b7
Jakob Færch <jrf@trifork.com>
2012-04-12 21:49:06 +0200

    Display correct greeting

--- rest-server/src/main/java/com/trifork/MyResource.java
+++ rest-server/src/main/java/com/trifork/MyResource.java
@@ -18,6 +18,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Jersey is working still";
+        return "Jersey is working";
     }
 }
